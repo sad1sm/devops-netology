79840ce866a14afbf49c9b10d0b4107a969a9e16
Alexey Zhavoronkov <azhavoronkov@backservice.ru>
2022-10-25 14:58:15 +0300

    Homework 09_03

---
+++ homework/09_03/example/fail.py
@@ -0,0 +1,15 @@
+from threading import local
+
+
+def increment(local_index):
+    local_index += 1
+    return local_index
+def get_square(numb):
+    return numb*numb
+def print_numb(numb):
+    print("Number is {}".format(numb))
+
+index = 0
+while (index < 10):
+    index = increment(index)
+    print(get_square(index))
